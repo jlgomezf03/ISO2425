0eec967fc40bf6996cf8789a211677a325d24f4e
jlgomezf03 <gomezfernandezjoseluis20@gmail.com>
2024-11-18 09:52:15 +0100

    Add files via upload

---
+++ ProcesosPorLotes/mkprofile.bat
@@ -0,0 +1,11 @@
+@echo off
+rem Script de Prueba
+rem Autor: José Luis Gómez Fernández
+rem Fecha: 18-11-2024
+
+mkdir %userprofile%\asir %userprofile%\dam %userprofile%\asir %userprofile%\daw %userprofile%\ceti %userprofile%\smr
+mkdir %userprofile%\asir\docs mkdir %userprofile%\asir\pract mkdir %userprofile%\asir\exam
+mkdir %userprofile%\dam\docs mkdir %userprofile%\dam\pract mkdir %userprofile%\dam\exam
+mkdir %userprofile%\ceti\docs mkdir %userprofile%\ceti\pract mkdir %userprofile%\ceti\exam 
+mkdir %userprofile%\smr\docs mkdir %userprofile%\smr\pract mkdir %userprofile%\smr\exam 
+
