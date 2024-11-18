e4a38ded63251e30a045eea2f8561224005f6749
jlgomezf03 <gomezfernandezjoseluis20@gmail.com>
2024-11-18 10:55:45 +0100

    Add files via upload

---
+++ ProcesosPorLotes/Media.bat
@@ -0,0 +1,11 @@
+@echo off
+Rem Script: Media
+Rem Autor: José Luis Gómez Fernández
+Rem Fecha: 18-11-2024
+
+set /a suma=%1 + %2 + %3
+
+set /a media=(%1 + %2 + %3)/3
+
+echo La media de %1 %2 y %3 es %media%
+
