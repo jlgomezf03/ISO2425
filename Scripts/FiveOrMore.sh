b27029206d233ab8447d450f74c7fa9485680d46
jlgomezf03 <gomezfernandezjoseluis20@gmail.com>
2025-02-11 10:17:42 +0100

    Add files via upload

---
+++ Scripts/FiveOrMore.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# Script de FiveOrMore
+# Autor: Jose Luis Gomez Fernandez
+# Fecha: 11/02/2025
+
+clear
+
+if [ $# -ne 2 ]; then
+
+	echo "Introduce dos parametros"
+	exit
+
+fi
+
+if [ -f $1 ]; then
+
+	echo "El fichero no debe de existir hay que borrarlo"
+	exit
+
+fi
+
+if [ ! -d $2 ]; then
+
+	echo "El directorio debe de existir"
+
+fi
+
+arch=$(ls $2 | wc -l)
+
+if [ $arch -eq 0 ]; then
+
+	echo "Directorio $2 vacío"
+	exit
+
+fi
+
+echo "Jose Luis Gomez Fernandez"
+
+ficheros=$(ls $2/*.txt)
+
+for i in $ficheros/*txt; do
+	if[ -f $i ]; then
+		lineas=$(cat $i | wc -l)
+		if [ $lineas -ge 5 ]; then
+			echo $i
+			echo $i >> $1
+			palabras=$(cat $i | wc -w)
+			echo "El fichero tiene $palabras palabras" >$i.q
+			cat $i >> $i.q
+
+		fi
+
+	 fi
+
+done
+
+numero=$(cat $1 | wc -l)
+
+echo $numero >> $1
